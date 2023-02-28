2cd22b9eaa064a989a2776c36aabb5a16757b5d1
wendtvi <vitoriawendt@gmail.com>
2023-02-28 16:54:10 -0300

    Update DiD_modeling.r

--- DiD_modeling.r
+++ DiD_modeling.r
@@ -11,3 +11,10 @@ ATT_Y=ATT_lin=prod(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[m
       prod(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real[merge_data_final$aliquota_real<=1.01]>=0.97]==2002]^(1/length(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real[merge_data_final$aliquota_real<=1.01]>=0.97]==2002])))/
       prod(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real[merge_data_final$aliquota_real<=1.01]>=0.97]==2001]^(1/length(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real[merge_data_final$aliquota_real<=1.01]>=0.97]==2001])))
   )  
+
+ATT_Y_arit=ATT_lin=mean(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real<=0.87]==2002])-
+  (
+    mean(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real<=0.87]==2001])*
+      mean(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real[merge_data_final$aliquota_real<=1.01]>=0.97]==2002])/
+      mean(merge_data_final$PC_TT_UN[merge_data_final$ANO.DO.EXERCICIO[merge_data_final$aliquota_real[merge_data_final$aliquota_real<=1.01]>=0.97]==2001])
+  )  
