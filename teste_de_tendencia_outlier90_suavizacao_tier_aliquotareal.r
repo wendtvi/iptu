edb440b99d29393ff7db71418b525aab698e1992
wendtvi <vitoriawendt@gmail.com>
2023-02-28 11:23:42 -0300

    Update teste_de_tendencia_outlier90_suavizacao_tier_aliquotareal.r

--- teste_de_tendencia_outlier90_suavizacao_tier_aliquotareal.r
+++ teste_de_tendencia_outlier90_suavizacao_tier_aliquotareal.r
@@ -11,7 +11,7 @@ vetor_cores=c("red","green","yellow", "blue", "pink", "gray", "orange", "cyan", 
 for (i in 2:length(vetor_percentis)){
   tiert0_inf=vetor_percentis[i-1]
   tiert1_inf=vetor_percentis[i]
-
+  
   tiert0_sup=vetor_percentis[length(vetor_percentis)-i+1]
   tiert1_sup=vetor_percentis[length(vetor_percentis)]
   
@@ -47,9 +47,9 @@ for (i in 2:length(vetor_percentis)){
   
   
   inf_vetor_precos_lan_tempo=c(inf_preco_lan_medio_1995,inf_preco_lan_medio_1996,inf_preco_lan_medio_1997,inf_preco_lan_medio_1998,inf_preco_lan_medio_1998,
-                           inf_preco_lan_medio_2000,inf_preco_lan_medio_2001,inf_preco_lan_medio_2002,
-                           inf_preco_lan_medio_2003,inf_preco_lan_medio_2004,inf_preco_lan_medio_2005,inf_preco_lan_medio_2006,
-                           inf_preco_lan_medio_2007,inf_preco_lan_medio_2008)
+                               inf_preco_lan_medio_2000,inf_preco_lan_medio_2001,inf_preco_lan_medio_2002,
+                               inf_preco_lan_medio_2003,inf_preco_lan_medio_2004,inf_preco_lan_medio_2005,inf_preco_lan_medio_2006,
+                               inf_preco_lan_medio_2007,inf_preco_lan_medio_2008)
   sup_vetor_precos_lan_tempo=c(sup_preco_lan_medio_1995,sup_preco_lan_medio_1996,sup_preco_lan_medio_1997,sup_preco_lan_medio_1998,sup_preco_lan_medio_1998,
                                sup_preco_lan_medio_2000,sup_preco_lan_medio_2001,sup_preco_lan_medio_2002,
                                sup_preco_lan_medio_2003,sup_preco_lan_medio_2004,sup_preco_lan_medio_2005,sup_preco_lan_medio_2006,
@@ -57,8 +57,8 @@ for (i in 2:length(vetor_percentis)){
   anos=c(1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008)
   
   ###Sem corrigir inflação
-    plot(y=inf_vetor_precos_lan_tempo,x=anos,pch=21,main = "Média geométrica valor de lançamento por ano",ylim = c(100000,210000))
-    abline(v=2001,col="red")
+  plot(y=inf_vetor_precos_lan_tempo,x=anos,pch=21,main = "Média geométrica valor de lançamento por ano",ylim = c(100000,210000))
+  abline(v=2001,col="red")
   lines(y=inf_vetor_precos_lan_tempo,x=anos,col="red")
   lines(y=sup_vetor_precos_lan_tempo,x=anos,col="blue")
   legend(2004,120000,legend = c(paste("<",format(round(tiert1_inf, 2), nsmall = 2)),paste(">",format(round(tiert0_sup, 2), nsmall = 2))),col = c("red","blue"),cex = .7,fill  = c("red","blue"))
@@ -183,11 +183,11 @@ for (i in 2:length(vetor_percentis)){
   anos=c(1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008)
   
   ###Sem corrigir inflação
-  plot(y=inf_vetor_precos_lan_tempo,x=anos,pch=21,main = "Média geométrica valor de lançamento por ano",ylim = c(11,13),col="white")
+  plot(y=inf_vetor_precos_lan_tempo,x=anos,pch=21,main = "Log da média geométrica valor de lançamento por ano e percentil de alíq real",ylim = c(11,13),col="white")
   abline(v=2001,col="red")
   lines(y=inf_vetor_precos_lan_tempo,x=anos,col="red")
   lines(y=sup_vetor_precos_lan_tempo,x=anos,col="blue")
-  legend(2004,11.5,legend = c(paste("<",format(round(tiert1_inf, 2), nsmall = 2)),paste(">",format(round(tiert0_sup, 2), nsmall = 2))),col = c("red","blue"),cex = .7,fill  = c("red","blue"))
+  legend(2004,11.5,legend = c(paste("<",format(round(tiert1_inf, 2), nsmall = 2)),paste(">",format(round(tiert0_sup, 2), nsmall = 2))),col = c("red","blue"),cex = 1,fill  = c("red","blue"))
   
 }
 
