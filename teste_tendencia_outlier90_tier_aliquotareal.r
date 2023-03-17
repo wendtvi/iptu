62e438462a694a3a2e355ef36395f3140c9b7b76
wendtvi <vitoriawendt@gmail.com>
2023-03-17 14:04:36 -0300

    Update teste_tendencia_outlier90_tier_aliquotareal.r

--- teste_tendencia_outlier90_tier_aliquotareal.r
+++ teste_tendencia_outlier90_tier_aliquotareal.r
@@ -12,14 +12,14 @@ names(merge_data_final)=gsub("merge_data_final.",names(merge_data_final),replace
 
 #for (i in 1:nrow(merge_data_final)){
 #  for (k in 1:nrow(ipca)){
-#    if(ipca$YEAR[k]==merge_data_final$ANO.DO.EXERCICIO[i])merge_data_final$PC_TT_UN[i]=merge_data_final$PC_TT_UN[i]*ipca[k,4]
+#    if(ipca$YEAR[k]==merge_data_final$ANO_LAN[i])merge_data_final$PC_TT_UN[i]=merge_data_final$PC_TT_UN[i]*ipca[k,4]
 #  }
 #}
 
 
 
 #vetor_percentis=quantile(merge_data_final$aliquota_real, probs = seq(0,1,0.33))
-vetor_percentis=c(0,0.97,1.03,2)
+vetor_percentis=c(0,0.85,1.15,2)
 vetor_cores=c("red","green","blue", "blue", "darkgoldenrod4", "gray", "orange", "cyan", "chocolate","black")
 for (i in 2:length(vetor_percentis)){
   tiert0=vetor_percentis[i-1]
@@ -29,20 +29,20 @@ for (i in 2:length(vetor_percentis)){
   merge_data_final_temp=merge_data_final_temp[merge_data_final_temp$aliquota_real<tiert1,]
   
   
-  preco_lan_medio_1995=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1995]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1995])))
-  preco_lan_medio_1996=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1996]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1996])))
-  preco_lan_medio_1997=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1997]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1997])))
-  preco_lan_medio_1998=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1998]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1998])))
-  preco_lan_medio_1999=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1999]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1999])))
-  preco_lan_medio_2000=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2000]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2000])))
-  preco_lan_medio_2001=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2001]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2001])))
-  preco_lan_medio_2002=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2002]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2002])))
-  preco_lan_medio_2003=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2003]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2003])))
-  preco_lan_medio_2004=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2004]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2004])))
-  preco_lan_medio_2005=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2005]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2005])))
-  preco_lan_medio_2006=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2006]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2006])))
-  preco_lan_medio_2007=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2007]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2007])))
-  preco_lan_medio_2008=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2008]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2008])))
+  preco_lan_medio_1995=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1995]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1995])))
+  preco_lan_medio_1996=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1996]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1996])))
+  preco_lan_medio_1997=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1997]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1997])))
+  preco_lan_medio_1998=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1998]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1998])))
+  preco_lan_medio_1999=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1999]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1999])))
+  preco_lan_medio_2000=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2000]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2000])))
+  preco_lan_medio_2001=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2001]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2001])))
+  preco_lan_medio_2002=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2002]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2002])))
+  preco_lan_medio_2003=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2003]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2003])))
+  preco_lan_medio_2004=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2004]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2004])))
+  preco_lan_medio_2005=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2005]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2005])))
+  preco_lan_medio_2006=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2006]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2006])))
+  preco_lan_medio_2007=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2007]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2007])))
+  preco_lan_medio_2008=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2008]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2008])))
   
   
   vetor_precos_lan_tempo=c(preco_lan_medio_1995,preco_lan_medio_1996,preco_lan_medio_1997,preco_lan_medio_1998,preco_lan_medio_1998,
@@ -80,9 +80,9 @@ for (i in 2:length(vetor_percentis)){
   merge_data_final_temp=merge_data_final_temp[merge_data_final_temp$aliquota_real<tiert1,]
   
   
-  preco_lan_medio_2000=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2000]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2000])))
-  preco_lan_medio_2001=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2001]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2001])))
-  preco_lan_medio_2002=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2002]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2002])))
+  preco_lan_medio_2000=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2000]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2000])))
+  preco_lan_medio_2001=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2001]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2001])))
+  preco_lan_medio_2002=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2002]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2002])))
   
   
   vetor_precos_lan_tempo=c(
@@ -91,7 +91,7 @@ for (i in 2:length(vetor_percentis)){
   
   ###Sem corrigir inflação
   if (i==2){ 
-    plot(y=vetor_precos_lan_tempo,x=anos,pch=21,main = "Log da média geométrica valor de lançamento por ano",ylim = c(11.0,12.1),type = 'l')
+    plot(y=vetor_precos_lan_tempo,x=anos,pch=21,main = "Log da média geométrica valor de lançamento por ano",ylim = c(11.0,13),type = 'l')
     abline(v=2001,col="red")
   }
   lines(y=vetor_precos_lan_tempo,x=anos,col=vetor_cores[i-1])
@@ -119,20 +119,20 @@ for (i in 2:length(vetor_percentis)){
   merge_data_final_temp=merge_data_final_temp[merge_data_final_temp$aliquota_real<tiert1,]
   
   
-  preco_lan_medio_1995=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1995]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1995])))
-  preco_lan_medio_1996=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1996]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1996])))
-  preco_lan_medio_1997=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1997]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1997])))
-  preco_lan_medio_1998=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1998]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1998])))
-  preco_lan_medio_1999=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1999]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==1999])))
-  preco_lan_medio_2000=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2000]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2000])))
-  preco_lan_medio_2001=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2001]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2001])))
-  preco_lan_medio_2002=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2002]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2002])))
-  preco_lan_medio_2003=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2003]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2003])))
-  preco_lan_medio_2004=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2004]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2004])))
-  preco_lan_medio_2005=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2005]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2005])))
-  preco_lan_medio_2006=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2006]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2006])))
-  preco_lan_medio_2007=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2007]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2007])))
-  preco_lan_medio_2008=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2008]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO.DO.EXERCICIO==2008])))
+  preco_lan_medio_1995=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1995]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1995])))
+  preco_lan_medio_1996=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1996]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1996])))
+  preco_lan_medio_1997=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1997]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1997])))
+  preco_lan_medio_1998=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1998]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1998])))
+  preco_lan_medio_1999=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1999]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==1999])))
+  preco_lan_medio_2000=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2000]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2000])))
+  preco_lan_medio_2001=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2001]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2001])))
+  preco_lan_medio_2002=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2002]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2002])))
+  preco_lan_medio_2003=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2003]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2003])))
+  preco_lan_medio_2004=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2004]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2004])))
+  preco_lan_medio_2005=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2005]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2005])))
+  preco_lan_medio_2006=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2006]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2006])))
+  preco_lan_medio_2007=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2007]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2007])))
+  preco_lan_medio_2008=prod(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2008]^(1/length(merge_data_final_temp$PC_TT_UN[merge_data_final_temp$ANO_LAN==2008])))
   
   
   vetor_precos_lan_tempo=c(log(preco_lan_medio_1995),log(preco_lan_medio_1996),log(preco_lan_medio_1997),log(preco_lan_medio_1998),log(preco_lan_medio_1999),
